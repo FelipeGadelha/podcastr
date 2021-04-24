8751ca596024b8dd4eaf672e56f7589e346b71b4
Felipe Gadelha Diniz da Silva <felipe.gadelha@cybersolutions.com.br>
2021-04-24 19:16:34 -0300

    finished nlw

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,9 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 
   return (
     <div className={styles.homepage}>
-      <Head></Head>
+      <Head>
+        <title>Home | Podcastr</title>
+      </Head>
       <section className={styles.latestEpisodes}>
         <h2>Últimos lançamentos</h2>
         <ul>
